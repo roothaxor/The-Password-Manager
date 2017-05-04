21ca55e15bb27c36fe6318d446693912b093337a
SanJay <indianblogger786@gmail.com>
2017-05-05 04:25:17 +0600

    Add files via upload

---
+++ Database_View.bat
@@ -0,0 +1,6 @@
+@echo off
+color 07
+mode con:cols=85 lines=35
+title SECURE VAULT- LEVEL 6 ACCESS
+python nsa_view
+pause>nul
---
+++ Password_Generate.bat
@@ -0,0 +1,6 @@
+@echo off
+color 17
+mode con:cols=70 lines=14
+title Military Grade Password Generation v.4.0
+python c:/tools/pass/nsa_gen
+REM pipe for py
