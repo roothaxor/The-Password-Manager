57fd367cad1d41f395fcbe5c8711a4257ce42ba8
SanJay <indianblogger786@gmail.com>
2017-05-22 20:58:34 +0600

    Delete author.py

--- __files/author.py
+++
@@ -1,7 +0,0 @@
-ionfdsnfdisncdscnso = 'toor' #Main password for accessing the password manager
-
-#Dont delete and make new db file, use pre-made .db file raname it if you want
-filedir = r'C:\tools\pass\jesus.db' #Location of the database file ( Change it )
-
-panicpassword = 'roothaxor'  #Panic Password, Enter this and your .db file will be moved to C:/users/user/appdata/roaming
-cipherKey = 'thisaafakecipher' #Cipher Key for AES_CBC encryption ( must be 16 characters long )
