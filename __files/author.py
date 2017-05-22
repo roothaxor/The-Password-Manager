4daa7bb593fb557e7b152de10a5f8f623c96aa7a
SanJay <indianblogger786@gmail.com>
2017-05-22 20:59:04 +0600

    Add files via upload

---
+++ __files/author.py
@@ -0,0 +1,8 @@
+import os
+file = os.path.realpath(__file__)
+dirc = os.path.dirname(file).strip("__files")
+##################### Config's ##########################
+ionfdsnfdisncdscnso = 'toor' #Main password for accessing the password manager
+filedir = dirc+'jesus.db' #Location of the database file.
+panicpassword = 'roothaxor'  #Panic password, Enter this and your .db file will be moved to C:/users/user/appdata/roaming
+cipherKey = 'thisisfakecipher' #Cipher Key for AES_CBC encryption ( must be 16 characters long )
